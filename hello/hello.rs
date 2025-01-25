ced370aa2cc8c6105bce43441d7ba389a7712772
Gaurav Kumar <42230870+gaurav-kumar-gks@users.noreply.github.com>
2025-01-25 09:38:02 +0530

    sample print

--- hello/hello.rs
+++ hello/hello.rs
@@ -1,7 +1,20 @@
 fn main() {
     // compile -> "rustc hello.rs"
     // run -> "./hello"
-    
     // println! is macro
     println!("Hello, world!");
+
+    // various print
+    // format! (return formatted string)
+    // print! and println! (print! with new line)
+    // eprint! and eprintln!
+
+    println!(
+        "My name is {name:*>8}:{code:$>5}\nToday is {0}\n
+        I have b{1:b} cookies with me yay\n",
+        "Wednesday",
+        10,
+        name = "Alexa",
+        code = 1010
+    );
 }
